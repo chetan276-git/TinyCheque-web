104cfb83cfdc279eb3155d5ef44596b5ea4b2e3a
chetan mane <73518916+chetan276-git@users.noreply.github.com>
2025-06-14 18:23:36 +0530

    Fixed UI issues

--- src/components/icons/angle-arrow-down.tsx
+++ src/components/icons/angle-arrow-down.tsx
@@ -1,13 +1,17 @@
 export const AngleArrowDown = () => (
   <svg
-    className='w-4 h-4 ml-1'
-    fill='none'
-    stroke='currentColor'
+    width='19'
+    height='19'
     viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    className='w-4 h-4'
   >
     <path
-      d='M19 9l-7 7-7-7'
-      strokeWidth={2}
+      d='M6 9l6 6 6-6'
+      stroke='currentColor'
+      strokeWidth='2'
+      fill='none'
       strokeLinecap='round'
       strokeLinejoin='round'
     />
